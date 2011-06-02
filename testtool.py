5bd09f76a4b4d1862f6c2097581420241764964f
Sean Graham <grahams@sean-graham.com>
2011-06-02 10:26:46 -0400

    testtool now uses the config system

--- testtool.py
+++ testtool.py
@@ -7,8 +7,15 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
+from xml.dom import minidom
+
 from Newsletter import Newsletter
 from BasePostingAction import BasePostingAction
+
+config = None
+scriptPath = "/usr/share/wordpress/tool/boston"
+hostsDict = dict()
+locationsDict = dict()
 
 film = "Ignore this Posting: The Alan Smithee Story"
 filmURL = "http://en.wikipedia.org/wiki/Alan_Smithee"
@@ -21,15 +28,46 @@ showTime = "7:00pm"
 
 synopsis = "This is a test post, ignore it."
 
+# read the configuration xml
+def readConfig(filename):
+    document = minidom.parse(filename)
+
+    global clubCity
+    global clubURL
+
+    clubCity = document.getElementsByTagName("clubCity")[0].childNodes[0].data
+    clubURL = document.getElementsByTagName("clubURL")[0].childNodes[0].data
+
+    hosts = document.getElementsByTagName('host')
+
+    for host in hosts:
+        name = host.getElementsByTagName('name')[0].childNodes[0].data
+        image = host.getElementsByTagName('image')[0].childNodes[0].data
+
+        hostsDict[name] = image
+
+    locations = document.getElementsByTagName('location')
+
+    for location in locations:
+        name = location.getElementsByTagName('name')[0].childNodes[0].data
+        link = location.getElementsByTagName('link')[0].childNodes[0].data
+
+        locationsDict[name] = link
+
+    return document
+
+
 nl = Newsletter("Boston", "http://boston.sundaynightfilmclub.com/", film, 
                 filmURL, host, hostURL, location, locationURL,
                 wearing, showTime, synopsis)
 
-plugins = glob.glob("plugins/Goo*.py")
+plugins = glob.glob("plugins/Crai*.py")
 sys.path.append("plugins/")
 
 for x in plugins: 
     className = x.replace(".py","").replace("plugins/","")
+
+    config = readConfig(scriptPath + "/.postingtoolrc")
 
     foo = __import__(className, globals(), locals(), [''])
     bar = getattr(foo,className)
@@ -37,4 +75,5 @@ for x in plugins:
     obj = bar()
 
     print obj
-    obj.execute(None,nl)
+    obj.execute(config,nl)
+
